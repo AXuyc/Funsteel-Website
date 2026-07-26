9f5d3bb81be333be105ba578c443fc948e4efc63
Alan Xu <alanxuyc@gmail.com>
2026-07-26 03:04:39 +0100

    Minor edits and preparing for online launch

--- js/products.js
+++ js/products.js
@@ -12,7 +12,7 @@ export const PRODUCTS = [
         origin: 'India, Zimbabwe',
         specs: [
           ['Chemical Composition', 'Chromium(Cr): 60-70%, Carbon(C): 4-10%, Silicon(Si): ≤4%, Phosphorus(P): ≤0.03%, Sulfur(S): ≤0.04%'],
-          ['Granularity', '0-5mm, 5-15mm, 10-50mm, 10-80mm, 10-150mm'],
+          ['Granularity', '0-5mm, 5-15mm, 10-50mm, 10-80mm, 10-150mm, 50-60mm, 60-70mm'],
           ['Packing', '1 MT jumbo bags or bulk shipment'],
           ['Price', '<a href="contact-us.html">Contact us</a> for pricing']
         ]
@@ -23,8 +23,8 @@ export const PRODUCTS = [
         origin: 'Zimbabwe, China',
         specs: [
           ['Chemical Composition', 'Chromium(Cr): 52-65%, Carbon(C): 4~10%, Silicon(Si): ≤1%, Phosphorus(P): ≤0.04%, Sulfur(S): ≤0.06%, Titanium(Ti): ≤0.03%'],
-          ['Granularity', '6-30mm, 10-50mm, 10-100mm'],
-          ['Packing', 'Bulk vessel or 1 MT bags'],
+          ['Granularity', '10-50mm, 10-80mm, 10-150mm'],
+          ['Packing', '1 MT jumbo bags or bulk shipment'],
           ['Price', '<a href="contact-us.html">Contact us</a> for pricing']
         ]
       },
@@ -34,8 +34,8 @@ export const PRODUCTS = [
         origin: 'Zimbabwe, China',
         specs: [
           ['Chemical Composition', 'Chromium(Cr): 60-70%, Carbon(C): 4-10%, Silicon(Si): ≤2%, Phosphorus(P): ≤0.03%, Sulfur(S): ≤0.04%'],
-          ['Granularity', '0-10mm, 10-50mm, 10-100mm'],
-          ['Packing', '1 MT jumbo bags'],
+          ['Granularity', '10-50mm, 10-80mm, 10-150mm'],
+          ['Packing', '1 MT jumbo bags or bulk shipment'],
           ['Price', '<a href="contact-us.html">Contact us</a> for pricing']
         ]
       },
@@ -45,8 +45,8 @@ export const PRODUCTS = [
         origin: 'Zimbabwe, China',
         specs: [
           ['Chemical Composition', 'Chromium(Cr): 60-70%, Carbon(C): 4-10%, Silicon(Si): ≤4%, Phosphorus(P): ≤0.01%, Sulfur(S): ≤0.04%'],
-          ['Granularity', '0-10mm, 10-50mm, 10-100mm'],
-          ['Packing', '1 MT jumbo bags'],
+          ['Granularity', '10-50mm, 10-80mm, 10-150mm, 10-200mm'],
+          ['Packing', '1 MT jumbo bags or bulk shipment'],
           ['Price', '<a href="contact-us.html">Contact us</a> for pricing']
         ]
       }
@@ -64,8 +64,8 @@ export const PRODUCTS = [
         description: 'Medium-carbon ferrochrome offers a balance between high strength and lower oxidation potential. It is commonly used in the production of engineering steels or cast iron which require a balance of iron and chromium content.',
         specs: [
           ['Chemical Composition', 'Chromium(Cr): 50-70%, Carbon(C): 0.5-4%, Silicon(Si): ≤2%, Phosphorus(P): ≤0.03%, Sulfur(S): ≤0.03%'],
-          ['Granularity', '0-10mm, 10-50mm'],
-          ['Packing', '1 MT jumbo bags'],
+          ['Granularity', '10-60mm, 10-200mm'],
+          ['Packing', '1 MT jumbo bags or bulk shipment'],
           ['Price', '<a href="contact-us.html">Contact us</a> for pricing']
         ]
       }
@@ -82,9 +82,9 @@ export const PRODUCTS = [
         name: 'Standard Grade',
         description: 'Low-carbon ferrochrome is used in the production of stainless steel and other special steel where low impurity levels and low carbon content are critical. It enhances the quality of the finished product.',
         specs: [
-          ['Chemical Composition', 'Chromium(Cr): 50-70%, Carbon(C): ≤0.5%, Silicon(Si): ≤1.5%, Phosphorus(P): ≤0.03%, Sulfur(S): ≤0.03%'],
-          ['Granularity', '0-3mm, 3-10mm'],
-          ['Packing', '1 MT steel drums or jumbo bags'],
+          ['Chemical Composition', 'Chromium(Cr): 50-65%, Carbon(C): ≤0.5%, Silicon(Si): ≤2.0%, Phosphorus(P): ≤0.03%, Sulfur(S): ≤0.03%'],
+          ['Granularity', '10-60mm, 10-200mm'],
+          ['Packing', '1 MT jumbo bags or bulk shipment'],
           ['Price', '<a href="contact-us.html">Contact us</a> for pricing']
         ]
       }
@@ -101,9 +101,9 @@ export const PRODUCTS = [
         name: 'Standard Grade',
         description: 'Used in scenarios where extremely low carbon content is required, such as in the production of high-grade stainless steel and specialty alloys.',
         specs: [
-          ['Chemical Composition', 'Chromium(Cr): 60-65%, Carbon(C): ≤0.15%, Silicon(Si): ≤1.0%, Phosphorus(P): ≤0.03%, Sulfur(S): ≤0.03%'],
-          ['Granularity', '0-3mm, 3-10mm, 10-50mm'],
-          ['Packing', '1 MT steel drums'],
+          ['Chemical Composition', 'Chromium(Cr): 50-65%, Carbon(C): ≤0.15%, Silicon(Si): ≤2.0%, Phosphorus(P): ≤0.03%, Sulfur(S): ≤0.03%'],
+          ['Granularity', '10-60mm, 10-200mm'],
+          ['Packing', '1 MT jumbo bags or bulk shipment'],
           ['Price', '<a href="contact-us.html">Contact us</a> for pricing']
         ]
       }
@@ -120,9 +120,9 @@ export const PRODUCTS = [
         name: 'Standard Grade',
         description: 'It is used as an intermediate alloy in the process of making medium, low and micro carbon ferrochrome, as well as the deoxidiser of stainless steel and other specialty alloys',
         specs: [
-          ['Chemical Composition', 'Silicon(Si): >35%, Chromium(Cr): >30%, Carbon(C): 0.02~1.0%, Phosphorus(P): ≤0.04%, Sulfur(S): ≤0.01% '],
-          ['Granularity', '10-50mm, 10-80mm'],
-          ['Packing', '1 MT jumbo bags or bulk'],
+          ['Chemical Composition', 'Silicon(Si): >35%, Chromium(Cr): >30%, Carbon(C): 0.02~1.0%, Phosphorus(P): ≤0.04%, Sulfur(S): ≤0.04% '],
+          ['Granularity', '10-60mm, 10-200mm'],
+          ['Packing', '1 MT jumbo bags or bulk shipment'],
           ['Price', '<a href="contact-us.html">Contact us</a> for pricing']
         ]
       }
@@ -163,7 +163,7 @@ export const PRODUCTS = [
     slug: 'chromium',
     name: 'Chromium',
     tagline: 'High purity chromium(Cr) with almost no carbon content (97~99.3% chromium). It is valued for its high hardness, corrosion resistance.',
-    origins: ['Malaysia', 'India', 'South Africa'],
+    origins: ['China'],
     image: ['Cr_1.jpg'],
     type: [
       {
@@ -171,8 +171,8 @@ export const PRODUCTS = [
         description: 'High Purity Chromium has high hardness, high strength, and good ductility. It displays excellent stability at high temperatures and resistant to corrosion.',
         specs: [
           ['Chemical Composition', 'Chromium(Cr): ≥99%, Carbon(C): ≤0.02%, Silicon(Si): ≤0.30%, Phosphorus(P): ≤0.01%, Sulfur(S): ≤0.02%, Nitrogen(N): ≤0.05%'],
-          ['Granularity', '10-50mm, 10-80mm'],
-          ['Packing', '1 MT jumbo bags or bulk'],
+          ['Granularity', '10-60mm'],
+          ['Packing', '1 MT jumbo bags or 100kg steel drum'],
           ['Price', '<a href="contact-us.html">Contact us</a> for pricing']
         ]
       }
@@ -182,7 +182,7 @@ export const PRODUCTS = [
     slug: 'chromium-nitride',
     name: 'Chromium Nitride',
     tagline: 'Chromium Nitride (CrN) is very hard and extremely resistant to corrosion.',
-    origins: ['Malaysia', 'India', 'South Africa'],
+    origins: ['China'],
     image: ['CrN_1.jpg'],
     type: [
       {
@@ -191,7 +191,7 @@ export const PRODUCTS = [
         specs: [
           ['Chemical Composition', 'Chromium(Cr): ~60%, Nitrogen(N): ~8.3%, Carbon(C): ~0.027%, Silicon(Si): ~1%, Phosphorus(P): ~0.027%, Sulfur(S): ~0.025%'],
           ['Granularity', '10-60mm'],
-          ['Packing', '1 MT jumbo bags or bulk'],
+          ['Packing', '1 MT jumbo bags'],
           ['Price', '<a href="contact-us.html">Contact us</a> for pricing']
         ]
       }
